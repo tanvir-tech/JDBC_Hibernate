a4fdbc25a93b979a95e1916be5596a2185f1170b
tanvir-tech <tanvirah325@gmail.com>
2021-02-19 01:30:05 +0600

    Maven......JDBC- DML Query

---
+++ src/main/java/JDBC_Demo/DML_Query.java
@@ -0,0 +1,31 @@
+package JDBC_Demo;
+
+import java.sql.*;
+
+public class DML_Query {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String urlPreset = "jdbc:mysql://localhost:3306/";
+        String dataBaseName = "jdbctest";
+        String url = urlPreset + dataBaseName;
+
+        String userName = "root";
+        String passWord = "";
+
+        String Query = "SELECT * FROM student WHERE id=18043";
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url,userName,passWord);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(Query);
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println(name);
+
+        statement.close();
+        connection.close();
+
+    }
+}
